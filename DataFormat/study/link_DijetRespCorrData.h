259b02fb12e6f5e4f82fff30c0e4cbaeacf1e78a
Andrius Juodagalvis <andjuo@hotmail.com>
2014-10-16 16:09:09 +0300

    DataFormat/study/link_DijetRespCorrData.* definition of std::ostream<<(DijetRespCorrDatum). Linking of DijetRespCorrDatum and DijetRespCorrData classes

---
+++ DataFormat/study/link_DijetRespCorrData.h
@@ -0,0 +1,13 @@
+#ifndef link_DijetRespCorrData_H
+#define link_DijetRespCorrData_H
+
+#define __localRun
+#include "../interface/DijetRespCorrData.h"
+
+// ----------------------------------------------------------------------
+
+std::ostream& operator<<(std::ostream& out, const DijetRespCorrDatum &d);
+
+// ----------------------------------------------------------------------
+
+#endif
