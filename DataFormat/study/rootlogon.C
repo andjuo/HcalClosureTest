71cbb415c39eb591c6b37cb18043488319e325a2
Andrius Juodagalvis <andjuo@hotmail.com>
2014-10-16 16:19:36 +0300

    DataFormat/study/rootlogon.C the latest version to compile the modules

--- DataFormat/study/rootlogon.C
+++ DataFormat/study/rootlogon.C
@@ -1,3 +1,11 @@
 {
   gROOT->ProcessLine(".L dijet_PFNtuple.C+");
+  if (1) {
+    gROOT->ProcessLine(".L link_DijetRespCorrData.cc+");
+    gROOT->ProcessLine(".L DijetRespCorrDataExtended.cc+");
+  }
+  if (1) {
+    gROOT->ProcessLine(".L CPlot.cc+");
+    gROOT->ProcessLine(".L Analyzer.cc+");
+  }
 }
