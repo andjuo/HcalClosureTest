15199085c7f45526fbd671d6b352cafcbd034eb8
Andrius Juodagalvis <andjuo@hotmail.com>
2014-11-16 21:46:23 +0100

    DataFormat/test/link_GammaJetFit.* moved functions to GammaJetData.*

--- DataFormat/test/link_GammaJetFit.h
+++ DataFormat/test/link_GammaJetFit.h
@@ -9,21 +9,5 @@
 #include <TBranch.h>
 #include <TString.h>
 
-#include "helper.h"
-
-// -----------------------------------------------------------
-
-int LoadGammaJetEvents(const TString fname,
-		       const GammaJetCuts_t &cuts,
-		       GammaJetFitter_t &fitter,
-		       Long64_t maxEntries=-1);
-
-// -----------------------------------------------------------
-
-int GetEmptyTowers(const GammaJetFitter_t &fitter,
-		   std::vector<Int_t> &towers,
-		   double minWeight=0.);
-
-// -----------------------------------------------------------
-
+//#include "helper.h"
 #endif
