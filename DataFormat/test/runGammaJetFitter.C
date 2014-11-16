2d55f045450eeaccbf3cc9e33ddc759cb39f5fe7
Andrius Juodagalvis <andjuo@hotmail.com>
2014-11-16 21:52:51 +0100

    DataFormat/test/runGammaJetFitter.C minor update and plot jet energy distributions (uncorr vs corr)

--- DataFormat/test/runGammaJetFitter.C
+++ DataFormat/test/runGammaJetFitter.C
@@ -12,20 +12,32 @@ void runGammaJetFitter(const TString fname="gjet_toy1_model2.root",
   HERE("load ok");
 
   std::vector<Int_t> fixTowers;
-  if (!GetEmptyTowers(fitter,fixTowers, 10)) return;
+  if (!GetEmptyTowers(fitter,fixTowers, 1, 0.1,1)) return;
 
   printVec("fixTowers",fixTowers);
 
+  if (1) {
+    GammaJetFitAnalyzer_t anObj(&fitter);
+    TH1D *hTowCount=
+      anObj.plot_TowerEntryCount("hTowerCount",
+				 "Events in towers;iEta_{tow};count",
+				 1,0.1,1);
+    TCanvas *cTowCount= new TCanvas("cTowCount","cTowCount",600,600);
+    hTowCount->Draw("LPE");
+    cTowCount->Update();
+    //return;
+  }
+
   std::vector<Double_t> hcalCorrCf(NUMTOWERS,1.);
   if (0) {
-    TArrayD cf= convert(hcalCorrCf);
+    TArrayD cf0= convert(hcalCorrCf);
     std::cout << "init values " << hcalCorrCf << "\n";
     std::cout << " 1st " << fitter.at(0)->CalcDiffEt() << "\n";
-    std::cout << " cmp " << fitter.at(0)->CalcDiffEt(cf) << "\n";
+    std::cout << " cmp " << fitter.at(0)->CalcDiffEt(cf0) << "\n";
 
     const GammaJetEvent_t *e = fitter.at(0);
     std::cout << " Et " << e->GetTagETtot() << " vs " << e->GetProbeETtot() << "\n";
-    std::cout << " cmp " << e->GetTagETtot(cf) << " vs " << e->GetProbeETtot(cf) << "\n";
+    std::cout << " cmp " << e->GetTagETtot(cf0) << " vs " << e->GetProbeETtot(cf0) << "\n";
     return;
   }
 
@@ -54,6 +66,7 @@ void runGammaJetFitter(const TString fname="gjet_toy1_model2.root",
   //fixTowers.push_back(31);
 
   TH1D* h= fitter.doFit("hCorr","hCorr", hcalCorrCf, &fixTowers);
+  TArrayD cf= convert(hcalCorrCf);
 
   //printVec("hcalCorrCf ",hcalCorrCf);
 
@@ -66,7 +79,6 @@ void runGammaJetFitter(const TString fname="gjet_toy1_model2.root",
   }
 
   if (1) {
-    TArrayD cf= convert(hcalCorrCf);
     std::cout << "hcalCorrCf " << hcalCorrCf << "\n";
     std::cout << "cf " << cf << "\n";
 
@@ -88,8 +100,6 @@ void runGammaJetFitter(const TString fname="gjet_toy1_model2.root",
   }
 
   if (0) {
-    TArrayD cf= convert(hcalCorrCf);
-
     GammaJetFitAnalyzer_t analyzer(&fitter);
 
     TH2D* h2TowerEn_unCorr=
@@ -110,7 +120,7 @@ void runGammaJetFitter(const TString fname="gjet_toy1_model2.root",
     cEt1->Update();
   }
 
-  if (1) {
+  if (0) {
     GammaJetFitAnalyzer_t anObj(&fitter);
     TH2D* h2=anObj.plot_TowerFitProfile("h2TowFittedProfile",
 					"tower fit profile",
@@ -119,6 +129,30 @@ void runGammaJetFitter(const TString fname="gjet_toy1_model2.root",
     h2->Draw("COLZ");
     cx->Update();
     //return;
+  }
+
+  if (1) { // plot jet energy uncorr vs corr
+    int transverse=0;
+    int tag=0; // jet is the probe
+    int nBins=50;
+    double etMax=200;
+    GammaJetFitAnalyzer_t anObj(&fitter);
+    TH1D* h1_uncorr = anObj.plot_Energy("h1JetEnergyUncorr",
+			      "Jet energy (uncorr)",tag,transverse,NULL,
+					nBins,0,etMax);
+    TH1D *h1_corr = anObj.plot_Energy("h1JetEnergyCorr",
+				      "Jet energy (corr)",tag,transverse,&cf,
+				      nBins,0,etMax);
+    TCanvas *cx= new TCanvas("cJetEn","cJetEn",600,600);
+    plotTwoHistos(cx,"Jet energy",
+		  h1_uncorr,"uncorr",kBlack,"hist",
+		  h1_corr,"corr",kRed,"LP");
+    //printHisto(h1_uncorr);
+    //printHisto(h1_corr);
+    //h1_uncorr->Draw("hist");
+    //h1_corr->SetLineColor(kRed);
+    //h1_corr->Draw("LPsame");
+    cx->Update();
   }
 
 }
